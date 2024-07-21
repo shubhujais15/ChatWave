a3bea4a6de0fb6176a1a8070862c5e97e04f4ec5
shubhujais15 <shubhujaisedu15@gmail.com>
2024-07-21 19:05:36 +0530

    Added logout button

--- src/components/list/userinfo/Userinfo.jsx
+++ src/components/list/userinfo/Userinfo.jsx
@@ -1,22 +1,36 @@
+// Userinfo.jsx
 import { useUserStore } from "../../../lib/userStore";
-import "./userinfo.css"
+import { useChatStore } from "../../../lib/chatStore";
+import { auth } from "../../../lib/firebase";
+import { signOut } from "firebase/auth";
+import "./userinfo.css";
 
 const Userinfo = () => {
-  const {currentUser} = useUserStore();
+  const { currentUser } = useUserStore();
+  const { chatId } = useChatStore();
+
+  const handleLogout = async () => {
+    try {
+      await signOut(auth);
+    } catch (error) {
+      console.log("Error logging out: ", error);
+    }
+  };
+
   return (
     <div className="userinfo">
-        <div className="user">
-            <img src={currentUser.avatar || "/avatar.png"} alt="" />
-            <h2>{currentUser.username}</h2>
-        </div>
-        <div className="icons">
-            <img src="/more.png" alt="" />
-            <img src="/video.png" alt="" />
-            <img src="/edit.png" alt="" />
-
-        </div>
+      <div className="user">
+        <img src={currentUser.avatar || "/avatar.png"} alt="" />
+        <h2>{currentUser.username}</h2>
+      </div>
+      <div className="icons">
+        <img src="/more.png" alt="" />
+        <img src="/video.png" alt="" />
+        <img src="/edit.png" alt="" />
+        {!chatId && <button onClick={handleLogout}>Logout</button>}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Userinfo
+export default Userinfo;
